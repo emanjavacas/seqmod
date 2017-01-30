2dc66df265ebc3465121280de817f3ab6309986f
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-01-30 18:27:28 +0100

    generalized optimizer

--- optimizer.py
+++ optimizer.py
@@ -2,48 +2,62 @@ import math
 import torch.optim as optim
 
 
+def root(x, nth=1):
+    """
+    warning!: this is only exact for p values below 3.
+    """
+    return math.pow(x, 1/nth)
+
+
 class Optimizer(object):
-    def __init__(self, params, method, lr, max_grad_norm, lr_decay=1,
-                 start_decay_at=None):
-        self.params = list(params)  # careful: params may be a generator
-        self.last_ppl = None
+    def __init__(self, params, method, lr=1., threshold=5.,
+                 lr_decay=1, start_decay_at=None):
+        self.params = list(params)
         self.lr = lr
-        self.max_grad_norm = max_grad_norm
-        self.method = method
+        self.threshold = threshold
         self.lr_decay = lr_decay
         self.start_decay_at = start_decay_at
+        self.method = method
+        self.last_ppl = None
         self.start_decay = False
-        self.optimizer = getattr(optim, self.method)(self.params, lr=self.lr)
+        self.optim = getattr(optim, self.method)(self.params, lr=self.lr)
 
-    def step(self):
-        # Compute gradients norm.
-        grad_norm = 0
-        for param in self.params:
-            grad_norm += math.pow(param.grad.data.norm(), 2)
+    def LP(self, p=1):
+        """
+        computes the l_p norm over gradients l_p norms
+        """
+        norms = sum(math.pow(pm.grad.data.norm(p=p), p) for pm in self.params)
+        return root(norms, nth=p)
 
-        grad_norm = math.sqrt(grad_norm)
-        shrinkage = self.max_grad_norm / grad_norm
+    def L2(self):
+        return self.LP(p=2)
 
-        for param in self.params:
-            if shrinkage < 1:
+    def L1(self):
+        return self.LP(p=1)
+
+    def clip_gradients(self, norm):
+        grad_norm = getattr(self, norm)()
+        if grad_norm > self.threshold:
+            shrinkage = self.threshold / grad_norm
+            for param in self.params:
                 param.grad.data.mul_(shrinkage)
 
-        self.optimizer.step()
-        return grad_norm
+    def step(self, norm='L2'):
+        self.clip_gradients(norm)
+        self.optim.step()
 
-    def update_learning_rate(self, ppl, epoch):
+    def maybe_update_lr(self, epoch, ppl):
         """
         Decay learning rate if validation perplexity does not improve
         or we hit the start_decay_at limit
         """
-        if self.start_decay_at is not None and epoch >= self.start_decay_at:
-            self.start_decay = True
-        if self.last_ppl is not None and ppl > self.last_ppl:
-            self.start_decay = True
-
-        if self.start_decay:
-            self.lr = self.lr * self.lr_decay
-            print("Decaying learning rate to %g" % self.lr)
-
-        self.last_ppl = ppl
-        self.optimizer = getattr(optim, self.method)(self.params, lr=self.lr)
+        if self.method == 'SGD':
+            if self.start_decay_at and epoch >= self.start_decay_at:
+                self.start_decay = True
+            if self.last_ppl is not None and ppl > self.last_ppl:
+                self.start_decay = True
+            if self.start_decay:
+                self.lr = self.lr * self.lr_decay
+                print("Decaying learning rate to %g" % self.lr)
+            self.last_ppl = ppl
+            self.optim = getattr(optim, self.method)(self.params, lr=self.lr)
