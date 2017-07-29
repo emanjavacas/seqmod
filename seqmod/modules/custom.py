b5fc0ce9c4ca52072adb3b2e5722558d6cdc065c
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-07-29 19:08:05 +0200

    added layer normalized gru

--- seqmod/modules/custom.py
+++ seqmod/modules/custom.py
@@ -6,7 +6,11 @@ from torch.autograd import Variable
 
 # Stateful Modules
 class _StackedRNN(nn.Module):
-    def __init__(self, cell, num_layers, in_dim, hid_dim, dropout=0.0):
+    def __init__(self, cell, num_layers, in_dim, hid_dim,
+                 dropout=0.0, **kwargs):
+        """
+        cell: str or custom cell class
+        """
         super(_StackedRNN, self).__init__()
         self.in_dim = in_dim
         self.hid_dim = hid_dim
@@ -17,9 +21,10 @@ class _StackedRNN(nn.Module):
         self.num_layers = num_layers
         self.layers = nn.ModuleList()
 
-        cell = getattr(nn, cell)
+        if isinstance(cell, str):
+            cell = getattr(nn, cell)
         for i in range(num_layers):
-            self.layers.append(cell(in_dim, hid_dim))
+            self.layers.append(cell(in_dim, hid_dim, **kwargs))
             in_dim = hid_dim
 
     def forward(self, inp, hidden):
@@ -68,7 +73,7 @@ class StackedGRU(_StackedRNN):
     def forward(self, inp, hidden):
         h_1 = []
         for i, layer in enumerate(self.layers):
-            h_1_i = layer(inp, hidden[0][i])
+            h_1_i = layer(inp, hidden[i])
             inp = h_1_i
             # dropout on all but last layer
             if i + 1 != self.num_layers and self.has_dropout:
@@ -76,6 +81,133 @@ class StackedGRU(_StackedRNN):
             h_1 += [h_1_i]
 
         return inp, torch.stack(h_1)
+
+
+class StackedNormalizedGRU(_StackedRNN):
+    def __init__(self, *args, **kwargs):
+
+        super(StackedNormalizedGRU, self).__init__(
+            NormalizedGRUCell, *args, **kwargs)
+
+    def forward(self, inp, hidden):
+        h_1 = []
+        for i, layer in enumerate(self.layers):
+            h_1_i = layer(inp, hidden[i])
+            inp = h_1_i
+            # dropout on all but last layer
+            if i + 1 != self.num_layers and self.has_dropout:
+                inp = self.dropout(inp)
+            h_1 += [h_1_i]
+
+        return inp, torch.stack(h_1)
+
+
+# Layer Normalization
+class NormalizedGRUCell(nn.GRUCell):
+    """
+    Layer-normalized GRUCell
+
+    kindly taken from here [https://github.com/pytorch/pytorch/issues/1959]
+    """
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(NormalizedGRUCell, self).__init__(input_size, hidden_size, bias)
+        # match GRUCell params for gates (reset, update) and input
+        self.gamma_ih = nn.Parameter(torch.ones(3 * self.hidden_size))
+        self.gamma_hh = nn.Parameter(torch.ones(3 * self.hidden_size))
+        self.eps = 0
+
+    def _layer_norm_x(self, x, g, bias=None):
+        mean = x.mean(1).expand_as(x)
+        std = x.std(1).expand_as(x)
+        output = g.expand_as(x) * ((x - mean) / (std + self.eps))
+        if bias is not None:
+            output += bias.expand_as(x)
+        return output
+
+    def _layer_norm_h(self, x, g, bias=None):
+        mean = x.mean(1).expand_as(x)
+        output = g.expand_as(x) * (x - mean)
+        if bias is not None:
+            output += bias.expand_as(x)
+
+        return output
+
+    def forward(self, x, h):
+        """
+        x: (batch x input_size)
+        h: (batch x hidden_size)
+        """
+        # 1. GATES
+        # 1.1. input to hidden gates
+        weight_ih_rz = self.weight_ih.narrow(  # update & reset gate params
+            0, 0, 2 * self.hidden_size
+        ).transpose(0, 1)
+        ih_rz = self._layer_norm_x(
+            torch.mm(x, weight_ih_rz),
+            self.gamma_ih.narrow(0, 0, 2 * self.hidden_size),
+            None if not self.bias
+            else self.bias_ih.narrow(0, 0, 2 * self.hidden_size))
+
+        # 1.2 hidden to hidden gates
+        weight_hh_rz = self.weight_hh.narrow(
+            0, 0, 2 * self.hidden_size
+        ).transpose(0, 1)
+        hh_rz = self._layer_norm_h(
+            torch.mm(h, weight_hh_rz),
+            self.gamma_hh.narrow(0, 0, 2 * self.hidden_size),
+            None if not self.bias
+            else self.bias_hh.narrow(0, 0, 2 * self.hidden_size))
+
+        rz = torch.sigmoid(ih_rz + hh_rz)
+        r = rz.narrow(1, 0, self.hidden_size)
+        z = rz.narrow(1, self.hidden_size, self.hidden_size)
+
+        # 1. PROJECTIONS
+        # 1.1. input to hidden projection
+        weight_ih_n = self.weight_ih.narrow(
+            0, 2 * self.hidden_size, self.hidden_size
+        ).transpose(0, 1)
+        ih_n = self._layer_norm_x(
+            torch.mm(x, weight_ih_n),
+            self.gamma_ih.narrow(0, 2 * self.hidden_size, self.hidden_size),
+            None if not self.bias
+            else self.bias_ih.narrow(0, 2*self.hidden_size, self.hidden_size))
+
+        # 1.2. hidden to hidden projection
+        weight_hh_n = self.weight_hh.narrow(
+            0, 2 * self.hidden_size, self.hidden_size
+        ).transpose(0, 1)
+        hh_n = self._layer_norm_h(
+            torch.mm(h, weight_hh_n),
+            self.gamma_hh.narrow(0, 2 * self.hidden_size, self.hidden_size),
+            None if not self.bias
+            else self.bias_hh.narrow(0, 2*self.hidden_size, self.hidden_size))
+
+        # h' = (1 - z) * n + (z * h)
+        n = torch.tanh(ih_n + r * hh_n)
+        h = (1 - z) * n + z * h
+        return h
+
+
+class NormalizedGRU(StackedNormalizedGRU):
+    def __init__(self, input_size, hid_dim, num_layers,
+                 dropout=0.0, bias=True, **kwargs):
+        if 'batch_first' in kwargs or 'bidirectional' in kwargs:
+            raise NotImplementedError
+        super(NormalizedGRU, self).__init__(
+            num_layers, input_size, hid_dim,
+            bias=bias, dropout=dropout)
+
+    def forward(self, xs, h_0):
+        """
+        xs: (seq_len x batch x input_size)
+        h_0: (num_layers * 1 x batch x hidden_size)
+        """
+        outputs, h_t = [], h_0
+        for x_t in xs:
+            output, h_t = super(NormalizedGRU, self).forward(x_t, h_t)
+            outputs.append(output)
+        return torch.stack(outputs), h_t
 
 
 class MaxOut(nn.Module):
