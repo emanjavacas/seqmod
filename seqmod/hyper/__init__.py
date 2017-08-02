46c81c8a94c4467c4e2d293395006d6d1b51aa20
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-08-02 17:58:17 +0200

    added hyper utilities

---
+++ seqmod/hyper/__init__.py
@@ -0,0 +1,2 @@
+from seqmod.hyper.utils import *
+from seqmod.hyper.hyperband import Hyperband
---
+++ seqmod/hyper/utils.py
@@ -0,0 +1,41 @@
+
+from hyperopt import hp
+from hyperopt.pyll.stochastic import sample
+
+
+def make_sampler(space):
+    """
+    Transforms a space in format:
+        {'param': ['hyperopt function name', type, hp_args...]}
+    into a proper hyperopt space, and returns a closure that knows
+    how to sample from the original space with proper conversion
+    to the predefined input type
+    """
+    hp_space = {k: getattr(hp, hp_fn)(k, *tuple(args))
+                for k, (hp_fn, _, *args) in space.items()}
+
+    def fn():
+        sampled = sample(hp_space)
+        for k in space:
+            param_type = space[k][1]
+            sampled[k] = param_type(sampled[k])
+        return sampled
+
+    return fn
+
+
+def random_search_generator(space, n_iter):
+    """
+    space = {'lr': ['loguniform', float, math.log(0.00001), math.log(1)],
+             'hid_dim': ['loguniform', int, math.log(10), math.log(1000)],
+             'emb_dim': ['choice', int, list(range(20, 60)]}
+    generator = random_search_generator(space)
+    next(generator)
+    >>> {'emb_dim': 46, 'hid_dim': 33, 'lr': 0.04909280719702653}
+    """
+    sampler = make_sampler(space)
+
+    counter = 0
+    while counter < n_iter:
+        yield sampler()
+        counter += 1
