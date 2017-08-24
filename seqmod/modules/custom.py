f45447c9bb3b80766892838c5ce0820cfe13bb26
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-08-24 16:08:03 +0200

    added sketch of RHNs

--- seqmod/modules/custom.py
+++ seqmod/modules/custom.py
@@ -1,7 +1,15 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
+
+
+def make_mask(inp, p, size):
+    keep_p = 1 - p
+    mask = inp.data.new(size)
+    mask.bernoulli_(keep_p).div_(keep_p)
+    return Variable(mask)
 
 
 # Stateful Modules
@@ -205,6 +213,198 @@ class NormalizedGRU(StackedNormalizedGRU):
             output, h_t = super(NormalizedGRU, self).forward(x_t, h_t)
             outputs.append(output)
         return torch.stack(outputs), h_t
+
+
+class RHN(nn.Module):
+    """
+    Implementation of the full RHN in which the recurrence is computed as:
+        s_l^t = (h_l^t * t_l^t) + (s_{l-1}^t * c_l^t)
+    Input embeddings to this module should not have dropout, since the
+    input dropout is controlled by the RHN in order to (optionally) enforce
+    same dropout mask is applied to all linear transformations (see argument)
+    `input_dropout`.
+
+    Parameters:
+    -----------
+    in_dim: int, number of features in the input
+    hid_dim: int, number of features in the output
+    num_layers: int, number of mini-steps inside a recurrence step.
+    tied_noise: bool, whether to use the same mask for all gates
+    input_dropout: float, dropout applied to the input layer
+    hidden_dropout: float, dropout applied to the recurrent layers
+    kwargs: extra arguments to general RNN cells that are ignored by RHN
+    """
+    def __init__(self, in_dim, hid_dim, num_layers=1, tied_noise=True,
+                 input_dropout=0.75, hidden_dropout=0.25, **kwargs):
+        self.in_dim = in_dim
+        self.hid_dim = hid_dim
+        self.depth = num_layers
+        self.input_dropout = input_dropout
+        self.hidden_dropout = hidden_dropout
+        self.tied_noise = tied_noise
+        super(RHN, self).__init__()
+        self.add_module('H', nn.Linear(in_dim, hid_dim))
+        self.add_module('T', nn.Linear(in_dim, hid_dim))
+        self.add_module('C', nn.Linear(in_dim, hid_dim))
+        self.rnn_h, self.rnn_t, self.rnn_c = [], [], []
+        for layer in range(self.depth):
+            bias = False if layer == 0 else True
+            rnn_h = nn.Linear(hid_dim, hid_dim, bias=bias)
+            rnn_t = nn.Linear(hid_dim, hid_dim, bias=bias)
+            rnn_c = nn.Linear(hid_dim, hid_dim, bias=bias)
+            self.add_module('rnn_H_{}'.format(layer + 1), rnn_h)
+            self.add_module('rnn_T_{}'.format(layer + 1), rnn_t)
+            self.add_module('rnn_C_{}'.format(layer + 1), rnn_c)
+            self.rnn_h.append(rnn_h)
+            self.rnn_t.append(rnn_t)
+            self.rnn_c.append(rnn_c)
+
+    def _step(self, H_t, T_t, C_t, h0, h_mask, t_mask, c_mask):
+        s_lm1 = h0
+        rnns = [self.rnn_h, self.rnn_t, self.rnn_c]
+        for l, (rnn_h, rnn_t, rnn_c) in enumerate(zip(rnns)):
+            s_lm1_H = h_mask.expand_as(s_lm1) * s_lm1
+            s_lm1_T = t_mask.expand_as(s_lm1) * s_lm1
+            s_lm1_C = c_mask.expand_as(s_lm1) * s_lm1
+            if l == 0:
+                H_t = F.tanh(H_t + rnn_h(s_lm1_H))
+                T_t = F.sigmoid(T_t + rnn_t(s_lm1_T))
+                C_t = F.sigmoid(C_t + rnn_t(s_lm1_C))
+            else:
+                H_t = F.tanh(rnn_h(s_lm1_H))
+                T_t = F.sigmoid(rnn_t(s_lm1_T))
+                C_t = F.sigmoid(rnn_t(s_lm1_C))
+            s_l = H_t * T_t + s_lm1 * C_t
+            s_lm1 = s_l
+
+        return s_l
+
+    def forward(self, inp, hidden=None):
+        """
+        Parameters:
+        -----------
+
+        inp: FloatTensor (seq_len x batch_size x in_dim)
+        hidden: FloatTensor (batch_size x hidden_size)
+        """
+        seq_len, batch_size, _ = inp.size()
+        mask_size = (batch_size, self.in_dim)
+        h_mask = make_mask(inp, self.input_dropout, mask_size)
+        if self.tied_noise:
+            t_mask, c_mask = h_mask, h_mask
+        else:
+            t_mask = make_mask(inp, self.input_dropout, (mask_size))
+            c_mask = make_mask(inp, self.input_dropout, (mask_size))
+        H = (h_mask.expand_as(inp) * inp).view(seq_len * batch_size, -1)
+        T = (t_mask.expand_as(inp) * inp).view(seq_len * batch_size, -1)
+        C = (c_mask.expand_as(inp) * inp).view(seq_len * batch_size, -1)
+        H = self.H(H).view(seq_len, batch_size, -1)
+        T = self.T(T).view(seq_len, batch_size, -1)
+        C = self.C(C).view(seq_len, batch_size, -1)
+
+        mask_size = (batch_size, self.hid_dim)
+        s_h_mask = make_mask(inp, self.hidden_dropout, mask_size)
+        if self.tied_noise:
+            s_t_mask, s_c_mask = s_h_mask, s_h_mask
+        else:
+            s_t_mask = make_mask(inp, self.hidden_dropout, mask_size)
+            s_c_mask = make_mask(inp, self.hidden_dropout, mask_size)
+
+        outs = []
+        for H_t, T_t, C_t in zip(H, T, C):
+            out = self._step(H_t, T_t, C_t, hidden,
+                             s_h_mask, s_t_mask, s_c_mask)
+            outs.append(out)
+            hidden = out
+
+        return torch.stack(outs), outs[-1]
+
+
+class RHNCoupled(nn.Module):
+    """
+    Simple variant of the RHN from https://arxiv.org/abs/1607.03474, in which
+    the carry gate is omitted and the highway transformation is defined as:
+        s_l^t = (h_l^t - s_{l-1}^t) * t_l^t + s_{l-1}^t
+    which corresponds to setting the carry gate as:
+        c = 1 - t
+
+    Input embeddings to this module should not have dropout, since the
+    input dropout is controlled by the RHN in order to (optionally) enforce
+    same dropout mask is applied to all linear transformations (see argument)
+    `input_dropout`.
+
+    Parameters: (See RHN)
+    """
+    def __init__(self, in_dim, hid_dim, num_layers=1, tied_noise=True,
+                 input_dropout=0.75, hidden_dropout=0.25, **kwargs):
+        self.in_dim = in_dim
+        self.hid_dim = hid_dim
+        self.depth = num_layers
+        self.input_dropout = input_dropout
+        self.hidden_dropout = hidden_dropout
+        self.tied_noise = tied_noise
+        super(RHNCoupled, self).__init__()
+        self.add_module('H', nn.Linear(in_dim, hid_dim))
+        self.add_module('T', nn.Linear(in_dim, hid_dim))
+        self.rnn_h, self.rnn_t = [], []
+        for layer in range(self.depth):
+            bias = False if layer == 0 else True
+            rnn_h = nn.Linear(hid_dim, hid_dim, bias=bias)
+            rnn_t = nn.Linear(hid_dim, hid_dim, bias=bias)
+            self.add_module('rnn_H_{}'.format(layer + 1), rnn_h)
+            self.add_module('rnn_T_{}'.format(layer + 1), rnn_t)
+            self.rnn_h.append(rnn_h), self.rnn_t.append(rnn_t)
+
+    def _step(self, H_t, T_t, h0, h_mask, t_mask):
+        s_lm1 = h0
+        for l, (rnn_h, rnn_t) in enumerate(zip(self.rnn_h, self.rnn_t)):
+            s_lm1_H = h_mask.expand_as(s_lm1) * s_lm1
+            s_lm1_T = t_mask.expand_as(s_lm1) * s_lm1
+            if l == 0:
+                H_t = F.tanh(H_t + rnn_h(s_lm1_H))
+                T_t = F.sigmoid(T_t + rnn_t(s_lm1_T))
+            else:
+                H_t = F.tanh(rnn_h(s_lm1_H))
+                T_t = F.sigmoid(rnn_t(s_lm1_T))
+            s_l = (H_t - s_lm1) * T_t + s_lm1
+            s_lm1 = s_l
+
+        return s_l
+
+    def forward(self, inp, hidden=None):
+        """
+        Parameters:
+        -----------
+
+        inp: FloatTensor (seq_len x batch_size x in_dim)
+        hidden: FloatTensor (batch_size x hidden_size)
+        """
+        seq_len, batch_size, _ = inp.size()
+        mask_size = (batch_size, self.in_dim)
+        h_mask = make_mask(inp, self.input_dropout, mask_size)
+        if self.tied_noise:
+            t_mask = h_mask
+        else:
+            t_mask = make_mask(inp, self.input_dropout, (mask_size))
+        H = (h_mask.expand_as(inp) * inp).view(seq_len * batch_size, -1)
+        T = (t_mask.expand_as(inp) * inp).view(seq_len * batch_size, -1)
+        H = self.H(H).view(seq_len, batch_size, -1)
+        T = self.T(T).view(seq_len, batch_size, -1)
+
+        mask_size = (batch_size, self.hid_dim)
+        s_h_mask = make_mask(inp, self.hidden_dropout, mask_size)
+        if self.tied_noise:
+            s_t_mask = s_h_mask
+        else:
+            s_t_mask = make_mask(inp, self.hidden_dropout, mask_size)
+
+        outs = []
+        for H_t, T_t in zip(H, T):
+            out = self._step(H_t, T_t, hidden, s_h_mask, s_t_mask)
+            outs.append(out)
+            hidden = out
+
+        return torch.stack(outs), outs[-1]
 
 
 class MaxOut(nn.Module):
