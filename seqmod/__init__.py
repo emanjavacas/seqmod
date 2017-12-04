c4ad079760b3ad4611827e3a606da7f6525fd7ca
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-12-04 18:34:23 +0100

    added misc submodule classes to misc (easier imports)

--- seqmod/__init__.py
+++ seqmod/__init__.py
@@ -2,3 +2,12 @@ from seqmod.modules import *
 from seqmod.misc import *
 from seqmod.utils import *
 from seqmod.loaders import *
+
+# shortcut (still keep the old import for backward compatibility)
+from seqmod.misc.beam_search import Beam
+from seqmod.misc.dataset import Dict, PairedDataset, CyclicBlockDataset
+from seqmod.misc.early_stopping import EarlyStopping, EarlyStoppingException
+from seqmod.misc.loggers import StdLogger, VisdomLogger
+from seqmod.misc.optimizer import Optimizer
+from seqmod.misc.preprocess import text_processor
+from seqmod.misc.trainer import Trainer
