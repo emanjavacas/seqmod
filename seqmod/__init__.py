d3e1d101e3c1911a65abc1d99f13c60ff85a97af
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-12-06 14:20:12 +0100

    fixed MultiDict import

--- seqmod/__init__.py
+++ seqmod/__init__.py
@@ -5,7 +5,7 @@ from seqmod.loaders import *
 
 # shortcut (still keep the old import for backward compatibility)
 from seqmod.misc.beam_search import Beam
-from seqmod.misc.dataset import Dict, PairedDataset, CyclicBlockDataset
+from seqmod.misc.dataset import Dict, MultiDict, PairedDataset, CyclicBlockDataset
 from seqmod.misc.early_stopping import EarlyStopping, EarlyStoppingException
 from seqmod.misc.loggers import StdLogger, VisdomLogger
 from seqmod.misc.optimizer import Optimizer
