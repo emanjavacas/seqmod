9523f89ddba1310047a8a75ac57bf34bb263aff1
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-01-10 14:02:49 +0100

    added conv modules

--- seqmod/modules/__init__.py
+++ seqmod/modules/__init__.py
@@ -2,4 +2,7 @@ from seqmod.modules.attention import *
 from seqmod.modules.ff import *
 from seqmod.modules.rnn import *
 from seqmod.modules.encoder_decoder import *
+from seqmod.modules.encoder import *
+from seqmod.modules.dcnn_encoder import DCNNEncoder
+from seqmod.modules.cnn_text_encoder import CNNTextEncoder
 from seqmod.modules.lm import *
